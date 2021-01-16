68b318cd5eefee5494cef733643b903459f38685
Leejiho-275 <leejh275@gmail.com>
2021-01-17 00:39:50 +0900

    [21.01.17] up&down 완료

--- 10. var와 let,const/4. up&down.js
+++ 10. var와 let,const/4. up&down.js
@@ -1,17 +1,44 @@
-// const randomNum = Math.floor(Math.random() * 100) + 1;
-let conunt = 0;
+let count = 1;
+const randomNum = Math.floor(Math.random() * 100) + 1;
+let begin = 1,
+    end = 100,
+    chan = 5;
 
-// let userNum = +prompt(`1~100사이의 수를 입력하세요.`);
+function chaNum(num) {
+    if (num > 0)
+        alert(`정답 기회가 ${chan}번 남았습니다.`);
+    else
+        alert(`정답 기회가 모두 소진되었습니다. 계속 문제를 풀어주세요.`);
+}
+
+function countAndChance() {
+    count++;
+    chan--;
+}
+
+console.log(randomNum);
+alert(`1 ~ 100 사이에서 선택된 숫자를 맞춰보세요!`);
+
 while (true) {
-    const randomNum = Math.floor(Math.random() * 100) + 1;
-    let userNum = +prompt(`1~100사이의 수를 입력하세요.`);
+    let userNum = +prompt(`숫자를 입력하세요. [${begin} ~ ${end}]`);
+    console.log(randomNum);
     if (randomNum < userNum) {
         alert('down!');
-        userNum = +prompt(`1~100사이의 수를 입력하세요.`);
-    } else if (randomNum > userNum) {
+        end = userNum;
+        countAndChance();
+        chaNum(chan);
+    } else if (randomNum === userNum) {
+        alert(`정답입니다! ${count}번 만에 맞췄습니다.`)
+        if (chan > 0) {
+            alert('YOU WIN!');
+        } else {
+            alert(`YOU LOSE!`);
+        }
+        break;
+    } else {
         alert('up!');
-        userNum = +prompt(`1~100사이의 수를 입력하세요.`);
-    } else if (randomNum === userNum) break;
-    count++;
+        begin = userNum;
+        countAndChance();
+        chaNum(chan);
+    };
 }
-alert(`${count}번 만에 맞췄습니다.`);
